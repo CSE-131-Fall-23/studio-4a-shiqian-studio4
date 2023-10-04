743a99a41c21a1f426345f2fc93fbb8b421447dd
shimufei <shimufei@localhost>
2023-10-04 16:50:25 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,25 @@
 package studio4;
 
-import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
+
+import java.awt.Color;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.clear();
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledEllipse(0.5, 0.6, 0.07, 0.07);
+		StdDraw.filledEllipse(0.5, 0.45, 0.11, 0.11);
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledEllipse(0.46, 0.6, 0.005, 0.005);
+		StdDraw.filledEllipse(0.54, 0.6, 0.005, 0.005);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,8 +1,9 @@
 package studio4;
 
+import java.util.Scanner;
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
@@ -19,6 +20,43 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		StdDraw.clear();
+		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		boolean fill = in.nextBoolean();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double m = in.nextDouble();
+		double n = in.nextDouble();
+		
+		if(shape.equals("rectangle")) {
+			if(fill == true) {
+				StdDraw.setPenColor(Color.PINK);
+				StdDraw.filledRectangle(x, y, m, n);
+			}
+			else {
+				StdDraw.setPenColor(Color.BLACK);
+				StdDraw.rectangle(x, y, m, n);
+			}
+		}
+		
+		else if(shape.equals("ellipse")) {
+			if(fill == true) {
+				StdDraw.setPenColor(Color.RED);
+				StdDraw.filledEllipse(x, y, m, n);
+			}
+			else {
+				Color purple = new Color(red, green, blue);
+				StdDraw.setPenColor(purple);
+				StdDraw.ellipse(x, y, m, n);
+			}
+		}
 		
 	}
 }
